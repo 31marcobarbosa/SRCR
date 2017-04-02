63377b4479168dca15ce7dca317e0523c4c3a87d
ricardocerto16 <a75315@alunos.uminho.pt>
2017-04-02 22:25:46 +0100

    Adição dos diferentes tipos de conhecimento Imperfeito

--- Exercicio2/exercicio2.pl
+++ Exercicio2/exercicio2.pl
@@ -40,13 +40,13 @@ utente( 5,'Carolina',50,'Rua do Caires','Braga','253987654').
 utente( 6,'Joana',26,'Av.da Boavista','Porto','961234567').
 utente( 7,'Fernando',65,'Rua do Loureiro','Viana do Castelo','966668569').
 utente( 8,'Rute',18,'Av. da Liberdade','Braga','916386423').
-utente( 9,'Maciel',45,'Rua das Flores','Porto','935731290').
-utente( 10,'Filipa',35,'Rua Padre Vitorino','Faro','289347681').
-utente( 11,'Mauro',76,'Rua Gil Vicente','Montalegre','276327904').
-utente( 12,'Laura',90,'Rua Fernando Mendes','Leiria','244000045').
-utente( 13,'Jaime',50,'Av. Norton Matos','Barcelos','914768180').
-utente( 14,'Lourenço',26,'Rua da Boavista','Guimaraes','926306127').
-utente( 15,'Tiago',16,'Rua Monsenhor de Melo','Vilamoura','936150873').
+utente( 9,'Maciel',45,rua_desconhecida,cidade_desconhecida,'935731290').
+utente( 10,'Filipa',35,rua_desconhecida,cidade_desconhecida,contacto_desconhecido).
+utente( 11,'Mauro',76,'Rua Gil Vicente','Montalegre',contacto_desconhecido).
+utente( 12,'Laura',90,'Rua Fernando Mendes',cidade_desconhecida,'244000045').
+utente( 13,'Jaime',50,rua_desconhecida,'Barcelos','914768180').
+utente( 14,'Lourenço',idade_desconhecida,'Rua da Boavista','Guimaraes','926306127').
+utente( 15,nome_desconhecido,16,'Rua Monsenhor de Melo','Vilamoura','936150873').
 
 
 % --------------------------------------------------------------
@@ -63,6 +63,7 @@ cuidado_prestado( 8,'Neurologia','Centro Hospitalar Sao Joao','Porto').
 cuidado_prestado( 9,'Urgência','Hospital de Braga','Braga').
 cuidado_prestado( 10,'Urgência','Hospital Lusiadas','Faro').
 cuidado_prestado( 11,'Otorrinolaringologia','Hospital de Vila Real','Vila Real').
+cuidado_prestado( 12,'Podologia',instituicao_desconhecida,'Vila Real').
 
 
 % --------------------------------------------------------------
@@ -82,10 +83,10 @@ atos( '01-01-17', 6, 6, 'Laranja', 'Dr.Reveillon', 50).
 atos( '03-03-17', 3, 1, 'Sem_pulseira', 'Dra.Candida', 45).
 atos( '08-03-17', 9, 9, 'Vermelha', 'Dr.Lima', 50).
 atos( '14-02-17', 14, 7, 'Amarela', 'Dra.Mafalda', 23).
-atos( '30-01-17', 7, 5, 'Sem_pulseira', 'Dr.Quimio', 10).
-atos( '01-02-17', 1, 6, 'Verde', 'Dra.Luisa', 25.5).
-atos( '01-03-17', 10, 6, 'Verde', 'Dra.Luisa', 25.5).
-atos( '10-03-17', 12, 11, 'Sem_pulseira', 'Dr.Luis', 12.50).
+atos( '30-01-17', 7, 5, cor_desconhecida, 'Dr.Quimio', custo_desconhecido).
+atos( '01-02-17', 1, 6, 'Verde', 'Dra.Luisa', custo_desconhecido).
+atos( '01-03-17', 10, 6, 'Verde', medico_desconhecido, 25.5).
+atos( '10-03-17', 12, 11, cor_desconhecida, 'Dr.Luis', 12.50).
 
 
 % --------------------------------------------------------------
@@ -96,6 +97,116 @@ atos( '10-03-17', 12, 11, 'Sem_pulseira', 'Dr.Luis', 12.50).
 		nao(excecao(utente(Id,Nome,Idade,Morada))).
 
 
+-atos(D,IdUt,IdServ,C,M,Ct) :- 
+	  nao(atos(D,IdUt,IdServ,C,M,Ct)) , 
+	  nao(excecao(atos(D,IdUt,IdServ,C,M,Ct))).
+
+
+-cuidado_prestado(Id,D,I,C) :- 
+				nao(cuidado_prestado(Id,D,I,C)) ,
+				nao(cuidado_prestado(excecao(Id,D,I,C))).
+
+% --------------------------------------------------------------
+% 
+% --------------------------------------------------------------
+
+% Extensao do meta-predicado demo: Questao,Resposta -> {V,F}
+
+demo( Questao,verdadeiro ) :- Questao.
+demo( Questao, falso ) :- -Questao.
+demo( Questao,desconhecido ) :-
+    nao( Questao ),
+    nao( -Questao ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do meta-predicado nao: Questao -> {V,F}
+
+nao( R ) :- R, !, fail.
+nao( R ).
+
+evolucao( Termo ) :-
+    solucoes( Invariante,+Termo::Invariante,Lista ),
+    insercao( Termo ),
+    teste( Lista ).
+
+insercao( Termo ) :-
+    assert( Termo ).
+insercao( Termo ) :-
+    retract( Termo ),!,fail.
+
+teste( [] ).
+teste( [R|LR] ) :-
+    R,
+    teste( LR ).
+
+solucoes( X,Y,Z ) :-
+    findall( X,Y,Z ).
+
+comprimento( S,N ) :-
+    length( S,N ).
+
+% ///////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+%					    INVARIANTES
+% ///////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+% Invariante Estrutural para utente:
+% (não permite a inserção de conhecimento repetido)
+
++utente(I,Nome,IDD,RU,CDD,CNT) :: (solucoes(I,(utente(I,_,_,_,_,_)),L),
+                        comprimento(L,N),
+                        N == 1).
+
+
+% --------------------------------------------------------------
+% Invariante Estrutural para cuidado_prestado:
+% (não permite a inserção de conhecimento repetido)
+
++cuidado_prestado(ID,D,I,X) ::(solucoes(ID,(cuidado_prestado(ID,_,_,_)),L),
+                                comprimento(L,N),
+                                 N == 1).
+
+% --------------------------------------------------------------
+% Invariante Estrutural para cuidado_prestado:
+% não permite a inserção de conhecimento repetido
+
++atos(D,IDUT,IDS,CP,MDC,C) :: (solucoes((D,IDUT,IDS),(atos(D,IDUT,IDS,_,_,_)),L),
+                              comprimento(L,N),
+                              N == 1).
+
+% ---------------------------------------------------------
+% Invariante que certifica a existência de um ID de utente e de um ID servico
+
++atos(D,IDUT,IDS,CP,MDC,C) :: (utente(IDUT,_,_,_,_,_),
+                              cuidado_prestado(IDS,_,_,_)).
+
+
+% ///////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+%					CONHECIMENTO INTERDITO
+% ///////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+% utente 16 tem contacto que ninguém pode conhecer
+
+utente( 16,'Djalo',30,'Rua Tangente II','Arouca',nxpto).
+excecao(utente(I,N,Id,R,C,Ct)) :- utente(I,N,Id,R,C,nxpto).
+nulo(nxpto).
++utente(I,N,Id,R,C,Ct) :: (solucoes((I,N,Id,R,C,Ct), (utente(16,'Djalo',30,'Rua Tangente II','Arouca',Ct),nao(nulo(Ct))),S), 
+							comprimento( S,N ) ,
+							N == 0).
+
+% ///////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+%					CONHECIMENTO IMPRECISO
+% ///////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+% um ato pode ter custado 50 ou 150 euros
+excecao(atos( '1-03-17', 12, 11, 'Sem_pulseira', 'Dr.Luisa', 50)).
+excecao(atos( '1-03-17', 12, 11, 'Sem_pulseira', 'Dr.Luisa', 150)).
+
+
+% ///////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+%					CONHECIMENTO INCERTO
+% ///////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+% UTENTES -----------------------------------------------------
 % nome desconhecido
 excecao(utente(I,N,Idd,R,C,Con)) :- 
 		utente(I,nome_desconhecido,Idd,R,C,Con).
@@ -125,12 +236,7 @@ excecao(utente(I,N,Idd,R,C,Con)) :-
 		utente(I,N,Idd,rua_desconhecida,cidade_desconhecida,Con).
 
 
-
-% --------------------------------------------------------------
-
--atos(D,IdUt,IdServ,C,M,Ct) :- 
-	  nao(atos(D,IdUt,IdServ,C,M,Ct)) , 
-	  nao(excecao(atos(D,IdUt,IdServ,C,M,Ct))).
+% ATOS ---------------------------------------------------------------
 
 
 % Cor Pulseira desconhecida
@@ -150,46 +256,9 @@ excecao(atos(D,IdUt,IdServ,C,M,Ct)) :-
 		 atos(D,IdUt,IdServ,cor_desconhecida,M,custo_desconhecido).
 
 
-% --------------------------------------------------------------
+% CUIDADOS PRESTADOS --------------------------------------------------
 
--cuidado_prestado(Id,D,I,C) :- 
-				nao(cuidado_prestado(Id,D,I,C)) ,
-				nao(cuidado_prestado(excecao(Id,D,I,C))).
 
 % Instituição desconhecida
 excecao(cuidado_prestado(Id,D,I,C)) :- 
 			cuidado_prestado(Id,D,instituicao_desconhecida,C).
-
-% --------------------------------------------------------------
-% 
-% --------------------------------------------------------------
-
-% Extensao do meta-predicado demo: Questao,Resposta -> {V,F}
-
-demo( Questao,verdadeiro ) :- Questao.
-demo( Questao, falso ) :- -Questao.
-demo( Questao,desconhecido ) :-
-    nao( Questao ),
-    nao( -Questao ).
-
-%--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Extensao do meta-predicado nao: Questao -> {V,F}
-
-nao( R ) :- R, !, fail.
-nao( R ).
-
-% ///////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-%					    INVARIANTES
-% ///////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-
-
-
-% ///////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-%					CONHECIMENTO INTERDITO
-% ///////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-% utente 16 tem contacto que ninguém pode conhecer
-
-utente( 16,'Djalo',30,'Rua Tangente II','Arouca',nxpto).
-excecao(utente(I,N,Id,R,C,Ct)) :- utente(I,N,Id,R,C,nxpto).
