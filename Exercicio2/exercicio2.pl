223c70f2c4774da0c0518d3815006de642787f8c
ricardocerto16 <a75315@alunos.uminho.pt>
2017-04-09 16:29:27 +0100

    juntei as cenas da parte 1

--- Exercicio2/exercicio2.pl
+++ Exercicio2/exercicio2.pl
@@ -131,7 +131,7 @@ remove(T) :- retract(T).
 inserir(E) :- assert(E).
 inserir(E) :- retract(E),!,fail.
 
-retroceder(E) :- solucoes(I,+E::I,L),
+retroceder(E) :- solucoes(I,-E::I,L),
                  teste(L),
                  remove(E).
 
@@ -378,3 +378,309 @@ excecao(atos(D,IdUt,IdServ,C,M,Ct)) :-
 % Instituição desconhecida
 excecao(cuidado_prestado(Id,D,I,C)) :- 
 			cuidado_prestado(Id,D,instituicao_desconhecida,C).
+
+
+
+% ///////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+%				 Programação em lógica					
+% ///////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+% -------------------------------------------------------------
+% Identificar os utentes por critérios de seleção 
+
+utenteID(ID,R) :- solucoes((ID,X,Y,Z,W,K),utente(ID,X,Y,Z,W,K),R).
+
+utenteNome(NM,R) :- solucoes((X,NM,Y,Z,W,K),utente(X,NM,Y,Z,W,K),R).
+
+utenteIdade(I,R) :- solucoes((X,Y,I,Z,W,K),utente(X,Y,I,Z,W,K),R).
+
+utenteRua(RU,R) :- solucoes((X,Y,Z,RU,W,K),utente(X,Y,Z,RU,W,K),R).
+
+utenteCidade(CDD,R) :- solucoes((X,Y,Z,W,CDD,K),utente(X,Y,Z,W,CDD,K),R).
+
+utenteContacto(CNT,R) :- solucoes((X,Y,Z,W,K,CNT),utente(X,Y,Z,W,K,CNT),R).
+
+% ---------------------------------------------------------
+% Predicado que junta duas listas numa nova lista
+% Extensao do predicado concat : L1, L2, L -> {V,F}
+
+concat([],L2,L2).
+concat(L1,[],L1).
+concat([X|L1],L2,[X|L]) :- concat(L1,L2,L).
+
+% -------------------------------------------------------------
+% Identificar as instituições prestadoras de cuidados de saúde
+% Extensao do predicado instCuidSaud : I -> {V,F}
+
+instCuidSaud(R) :- solucoes(I,cuidado_prestado(_,_,I,_),L),
+                   retiraRep(L,R).
+
+retiraRep([],[]).
+retiraRep([X|A],R) :- retiraEle(X,A,L),
+                      retiraRep(L,T),
+                      R = [X|T].
+
+retiraEle(A,[],[]).
+retiraEle(A,[A|Y],T) :- retiraEle(A,Y,T).
+retiraEle(A,[X|Y],T) :- X \== A,
+                        retiraEle(A,Y,R),
+                        T = [X|R].
+
+% -------------------------------------------------------------
+% Identificar os cuidados prestados por instituição
+% Extensao do predicado cuidInst : I, L -> {V,F}
+
+cuidInst(I,R) :- solucoes(C,cuidado_prestado(_,C,I,_),R).
+
+% -------------------------------------------------------------
+% Identificar os cuidados prestados por cidade
+% Extensao do predicado cuidCid : I, L -> {V,F}
+
+cuidCid(C,R) :- solucoes(S,cuidado_prestado(_,S,_,C),P),
+                retiraRep(P,R).
+
+% -------------------------------------------------------------
+% Identificar os utentes de uma Instituição
+% Extensao do predicado utentesInstituicao : I, L -> {V,F}
+
+utentesInstituicao(I,R) :- solucoes(S, cuidado_prestado(S,_,I,_), P),
+                           retiraRep(P,F),
+                           utServ(F,R).
+                           
+utServ([S],R) :- utentesServico(S,R).
+utServ([S|Ss],R) :- utentesServico(S,F),
+                    utServ(Ss,P),
+                    concat(F,P,R).
+
+% -------------------------------------------------------------
+% Identificar os utentes de um Serviço
+% Extensao do predicado utentesServico : I, L -> {V,F}
+
+utentesServico(S,R) :- solucoes(U, atos(_,U,S,_,_,_), P),
+                       retiraRep(P,F),
+                       idUt(F,R).
+
+idUt([U],R) :- utenteID(U,R).
+idUt([U|Us],R) :- utenteID(U,F),
+                  idUt(Us,P),
+                  concat(F,P,R).
+
+% -------------------------------------------------------------
+% Identificar os atos médicos realizados por utente
+% Extensao do predicado atoUte : I, L -> {V,F}
+
+atoUte(U,R) :- solucoes((X,U,Y,Z,W,Q), atos(X,U,Y,Z,W,Q), P),
+               retiraRep(P,R).
+
+% -------------------------------------------------------------
+% Identificar os atos médicos realizados por instituição
+% Extensao do predicado atoInst : I, L -> {V,F}
+
+atoInst(I,R) :- solucoes(S, cuidado_prestado(S,_,I,_), F),
+                retiraRep(F,P),
+                servAto(P,R).
+
+servAto([S],R) :- atoServ(S,R).
+servAto([S|Ss],R) :- atoServ(S,F),
+                     servAto(Ss,P),
+                     concat(F,P,R). 
+
+% -------------------------------------------------------------
+% Identificar os atos médicos realizados por serviço
+% Extensao do predicado atoServ : I, L -> {V,F}
+
+atoServ(S,R) :- solucoes((X,Y,S,Z,W,Q), atos(X,Y,S,Z,W,Q), R).
+
+% -------------------------------------------------------------
+% Determinar todas as instituições a que um utente recorreu 
+% Extensao do predicado nInstUte : I, L -> {V,F}
+
+nInstUte(U,R) :- nServUte(U,F),
+                 servicosInstituicao(F,P),
+                 retiraRep(P,R).
+
+servicosInstituicao([S],R) :- solucoes(I, cuidado_prestado(S,_,I,_), R).                    
+servicosInstituicao([S|Ss],R) :-  solucoes(I, cuidado_prestado(S,_,I,_), P),
+                                  servicosInstituicao(Ss,F),
+                                  concat(P,F,R).
+
+% -------------------------------------------------------------
+% Determinar todas os serviços a que um utente recorreu 
+% Extensao do predicado nServUte : I, L -> {V,F}
+
+nServUte(U,R) :- solucoes(S, atos(_,U,S,_,_,_), P),
+                 retiraRep(P,R).
+
+% -------------------------------------------------------------
+% Calcular o custo total dos atos médicos por utente 
+% Extensao do predicado custoUte : I, L -> {V,F}
+
+custoUte(U,R) :- atoUte(U,F),
+                 atoCusto(F,R).
+
+atoCusto([(_,_,_,_,_,C)],R) :- R is C.
+atoCusto([(_,_,_,_,_,C)|Cs],R) :- atoCusto(Cs,F),
+                              R is C+F.
+
+% -------------------------------------------------------------
+% Calcular o custo total dos atos médicos por serviço 
+% Extensao do predicado custoServ : I, L -> {V,F}
+
+custoServ(S,R) :- atoServ(S,F),
+                  atoCusto(F,R).
+
+% -------------------------------------------------------------
+% Calcular o custo total dos atos médicos por instituição
+% Extensao do predicado custoInst : I, L -> {V,F}
+
+custoInst(I,R) :- atoInst(I,F),
+                  atoCusto(F,R).
+
+% -------------------------------------------------------------
+% Calcular o custo total dos atos médicos por data 
+% Extensao do predicado custoData : I, L -> {V,F}
+
+custoData(D,R) :- solucoes((D,X,Y,Z,W,Q), atos(D,X,Y,Z,W,Q), F),
+                  atoCusto(F,R).
+
+% -------------------------------------------------------------
+% Registar utentes
+% Extensao do predicado registaUtentes : L,N,O,P -> {V,F}
+
+registaUtentes(ID,NM,I,RU,CDD,CNT) :- evolucao(utente(ID,NM,I,RU,CDD,CNT)).  
+
+% -------------------------------------------------------------
+% Registar cuidados
+% Extensao do predicado registaCuidados : L,M,N,O -> {V,F}
+
+registaCuidados(ID,D,I,C) :- evolucao(cuidado_prestado(ID,D,I,C)).
+
+% -------------------------------------------------------------
+% Registar atos médicos
+% Extensao do predicado registaAtos : L,M,N,O -> {V,F}
+
+registaAtos(D,IDUT,IDS,CP,MDC,C) :- evolucao(atos(D,IDUT,IDS,CP,MDC,C)).
+
+% -------------------------------------------------------------
+% Remover utentes
+% Extensao do predicado removeUtentes : L -> {V,F}
+
+removeUtentes(U) :- solucoes((D,U,IDS),atos(D,U,IDS,_,_,_),R),
+          removeTodosAtos(R),
+          retroceder(utente(U,N,I,RU,CDD,CNT)).
+
+removeTodosAtos([]).
+removeTodosAtos([(D,IDUT,IDS)]) :- removeAtos(D,IDUT,IDS).
+removeTodosAtos([(D,IDUT,IDS)|As]) :- removeAtos(D,IDUT,IDS),
+                        removeTodosAtos(As).
+
+% -------------------------------------------------------------
+% Remover cuidados
+% Extensao do predicado removeCuidados : L -> {V,F}
+
+removeCuidados(I) :- retroceder(cuidado_prestado(I,D,C,Cid)).
+
+% -------------------------------------------------------------
+% Remover atos médicos
+% Extensao do predicado removeAtos : L -> {V,F}
+
+removeAtos(D,IDUT,IDS) :- retroceder(atos(D,IDUT,IDS,_,_,_)).
+                               
+
+% ---------------------------------------------------------
+% EXTRAS
+% ----------------------------------------------------------
+% Numero de serviços de uma Instituição
+% Extensão do predicado numeroServicos : I , R -> {V,F}
+
+numeroServicos(I,R) :- solucoes(S, cuidado_prestado(S,_,I,_), P),
+                       comprimento(P,T),
+                       R is T.
+
+% Numero de Utentes
+% Extensão do predicado numeroUtentes : I -> {V,F}
+
+numeroUtentes(R) :- solucoes(U,utente(U,_,_,_,_,_),L),
+                    comprimento(L,T),
+                    R is T.
+
+% Numero de Atos
+% Extensão do predicado numeroAtos : I -> {V,F}
+
+numeroAtos(R) :- solucoes(A,atos(A,_,_,_,_,_),L),
+                 comprimento(L,T),
+                 R is T.
+
+
+% Numero de Cores de uma dada Pulseira
+% Extensão do predicado nCorPul : I , L -> {V,F}
+
+nCorPul(C,R) :- solucoes(C,atos(_,_,_,C,_,_),L),
+              comprimento(L,T),
+              R is T.
+
+
+% Cores das Pulseira
+% Extensão do predicado corPuls : I -> {V,F}
+
+corPuls(C,R) :- solucoes(C,atos(_,_,_,C,_,_),T),
+              retiraRep(T,R).
+
+% Ato Médico Mais Caro Registado até ao Momento
+% Extensão do predicado msCro : I -> {V,F}
+
+msCro(R) :- solucoes(Q,atos(_,_,_,_,_,Q),T),
+            maxLst(T,0,C),
+            solucoes((X,U,Y,Z,W,C), atos(X,U,Y,Z,W,C),K),
+            servAtos(K,R).
+
+servAtos([(_,_,S,_,_,_)],R) :- solucoes((S,B,C,D), cuidado_prestado(S,B,C,D), R).
+servAtos([(_,_,S,_,_,_)|Cs],R) :- solucoes((S,B,C,D), cuidado_prestado(S,B,C,D), P),
+                                  servAtos(Cs,F),
+                                  concat(P,F,R). 
+
+maxLst([C],K,R) :- maior(C,K,X),
+                   R is X.
+maxLst([C|Cs],K,R) :- maior(C,K,X),
+                      maxLst(Cs,X,R).
+
+maior(X,Y,X) :- X >= Y.
+maior(X,Y,Y) :- Y > X.
+
+
+% Ordenar os Atos Médicos Registados até ao Momento
+% Extensão do predicado ordCst : I -> {V,F}
+
+ordCst(R) :- solucoes(Q,atos(_,_,_,_,_,Q),T),
+             ordenaL(T,R). 
+
+
+ordenaL([X],[X]).
+ordenaL([H|T],OL) :- ordenaL(T,OT),
+                     insr(H,OT,OL).
+
+insr(X,[],[X]).
+insr(X,[H|T],[X,H|T]) :- X =< H.
+insr(X,[H|T],[H|NT]) :- X > H, 
+                        insr(X,T,NT).
+
+% Médicos de uma dada Instituição
+% Extensão do predicado medInst : I, R -> {V,F}
+
+medInst(I,R) :- solucoes(ID,(cuidado_prestado(ID,_,I,_)),L), 
+                getDoc(L,R).
+
+getDoc([],[]).
+getDoc([X|XS],RS) :- solucoes(M,atos(_,_,X,_,M,_),MS),
+                     getDoc(XS,TS),
+                     concat(MS,TS,F),
+                     retiraRep(F,RS).    
+  
+% Média dos custos dos atos
+% Extensão do precicado mediaCusto : I, R -> {V,F}
+
+mediaCusto(R) :- solucoes((X,U,Y,Z,W,Q), atos(X,U,Y,Z,W,Q), P),
+                 atoCusto(P,F),
+                 comprimento(P,K),
+                 R is F/K.
