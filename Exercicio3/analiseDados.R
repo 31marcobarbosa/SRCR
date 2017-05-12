a65e8525ffcc8a096ecbbad861e7e1e17b442aa0
ricardocerto16 <a75315@alunos.uminho.pt>
2017-05-12 17:22:09 +0100

    updating

--- Exercicio3/analiseDados.R
+++ Exercicio3/analiseDados.R
@@ -2,7 +2,8 @@ library("leaps")
 
 dados <- read.csv("C:/Users/rj3/OneDrive/Documentos/GitHub/SRCR/Exercicio3/exaustaoNorm.csv")
 
-resultado <- regsubsets(ExhaustionLevel ~ Performance.KDTMean + Performance.MAMean + 
+
+resultado <- regsubsets(Performance.Task ~ Performance.KDTMean + Performance.MAMean + 
                           Performance.MVMean + Performance.TBCMean + Performance.DDCMean + 
                           Performance.DMSMean + Performance.AEDMean + Performance.ADMSLMean,
                         dados,nvmax = 6)
@@ -11,15 +12,9 @@ summary(resultado)
 
 
 
+#resultado <- regsubsets(ExhaustionLevel ~ Performance.KDTMean + Performance.MAMean + 
+#                          Performance.MVMean + Performance.TBCMean + Performance.DDCMean + 
+#                          Performance.DMSMean + Performance.AEDMean + Performance.ADMSLMean,
+#                        dados,nvmax = 6)
 
 
-#library("arules")
-#library("matrix")
-#arules
-#x1 <- discretize(dados$LTI, method = "frequency",categories = 4)
-#plot(x1)
-
-#dados$income <- as.numeric(discretize(dados$income, method = "frequency",categories = 4))
-
-
-#leaps
